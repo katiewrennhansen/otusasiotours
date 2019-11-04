1c4bc94c5e87028f984d6b96aa4d6f0f108cf750
Katie Hansen <khansen0415@gmail.com>
2019-11-03 19:12:07 -0500

    add index styles and css grid

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-
-
 //DOM VARIABLES
 const toggleBtn = document.querySelector('button');
 const main = document.querySelector('main');
 const collapse = document.getElementById('navbar-collapse');
 const navLinks = document.querySelectorAll('.nav-link');
-
-
+const nav = document.querySelector('.navbar');
 
 
 
 toggleBtn.addEventListener('click', function(){
     toggleBtn.classList.toggle('cross');
     collapse.classList.toggle('toggle');
+    nav.classList.toggle('toggle-nav')
 });
 
 navLinks.forEach(link => {
