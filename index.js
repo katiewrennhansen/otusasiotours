23486041ccc98acc1175ba89e52a31724ab187c9
Katie Hansen <khansen0415@gmail.com>
2019-11-05 18:14:23 -0500

    add morocco past trip page and photo gallery

--- index.js
+++ index.js
@@ -19,3 +19,48 @@ navLinks.forEach(link => {
         toggleBtn.classList.remove('cross');
     });
 });
+
+
+
+
+
+
+//lightbox styles
+function openModal() {
+    document.getElementById("modal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("modal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("demo");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+
