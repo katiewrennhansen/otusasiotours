a9579fcf4479393f7ae561cd5370495b262e5a20
Katie Hansen <khansen0415@gmail.com>
2019-11-15 16:35:28 -0500

    update commit

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const main = document.querySelector('main');
 const collapse = document.getElementById('navbar-collapse');
 const navLinks = document.querySelectorAll('.nav-link');
 const nav = document.querySelector('.navbar');
-
+const modal = document.getElementById('modal');
+const slides = document.getElementsByClassName('slide');
+const dots = document.getElementsByClassName('demo');
 
 
 toggleBtn.addEventListener('click', function(){
@@ -22,45 +24,39 @@ navLinks.forEach(link => {
 
 
 
-
-
-
-//lightbox styles
+//open modal
 function openModal() {
-    document.getElementById("modal").style.display = "block";
-  }
+    modal.style.display = 'block';
+}
   
   // Close the Modal
-  function closeModal() {
-    document.getElementById("modal").style.display = "none";
-  }
+function closeModal() {
+    modal.style.display = 'none';
+}
   
-  var slideIndex = 1;
-  showSlides(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
   
   // Next/previous controls
-  function plusSlides(n) {
+function plusSlides(n) {
     showSlides(slideIndex += n);
-  }
+}
   
   // Thumbnail image controls
-  function currentSlide(n) {
+function currentSlide(n) {
     showSlides(slideIndex = n);
-  }
+}
   
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    var dots = document.getElementsByClassName("demo");
+function showSlides(n) {
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      slides[i].style.display = 'none';
     }
     for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(' active', '');
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-  }
+    slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].className += ' active';
+}
 
