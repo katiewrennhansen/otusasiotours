ba8c1d14e5e5886dea57bd2ab32df93703ebfebc
Katie Hansen <khansen0415@gmail.com>
2021-01-11 18:58:47 -0700

    update uganda, fix console error

--- index.js
+++ index.js
@@ -65,7 +65,10 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = 'none';
     }
-    slides[slideIndex-1].style.display = 'block';
+
+    if(slides[slideIndex-1]){
+      slides[slideIndex-1].style.display = 'block';
+    }
 }
 
 
